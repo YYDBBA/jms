e683aa4ae42c5d21a934fd0557ad73ce5f935a4e
YYDBBA <1806254330@qq.com>
2019-02-21 17:58:12 +0800

    add communi-tips

---
+++ src/components/communication/Tips.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="tips">
+      <div class="tips-item">
+        <h4 class="smallTip">最新资讯</h4>
+        <ul>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+        </ul>
+      </div>
+      <div class="tips-item">
+        <h4 class="smallTip">最新资讯</h4>
+        <ul>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+        </ul>
+      </div>
+      <div class="tips-item">
+        <h4 class="smallTip">最新资讯</h4>
+        <ul>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+          <li class="item">来啊</li>
+        </ul>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Tips"
+    }
+</script>
+
+<style scoped>
+
+  .tips {
+    position: absolute;
+    top: 150px;
+    left: 15%;
+    width: 300px;
+    min-height: 450px;
+    background-color: yellow;
+  }
+
+  .tips-item {
+    width: 100%;
+    height: 150px;
+  }
+
+  .smallTip {
+    padding-left: 5px;
+    width: 100%;
+    height: 30px;
+    line-height: 30px;
+    font-size: 15px;
+    box-sizing: border-box;
+  }
+  
+  .item {
+    float: left;
+    width: 100px;
+    height: 40px;
+    line-height: 40px;
+    font-size: 12px;
+    padding-left: 5px;
+    box-sizing: border-box;
+  }
+</style>
--- src/pages/Communication.vue
+++ src/pages/Communication.vue
@@ -1,19 +1,25 @@
 <template>
-  <div>
+  <div class="all">
     <communi-header></communi-header>
+    <communi-tips></communi-tips>
   </div>
 </template>
 
 <script>
   import CommuniHeader from './../components/communication/Header'
+  import CommuniTips from './../components/communication/Tips'
   export default {
     name: "Communication",
     components: {
-      CommuniHeader
+      CommuniHeader,
+      CommuniTips
     }
   }
 </script>
 
 <style scoped>
 
+  .all {
+    position: relative;
+  }
 </style>
